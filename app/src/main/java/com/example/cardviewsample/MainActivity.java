06aae4408d5290ce9419cf4bfc0eb9e719248b7e
tesoro01 <aqaqua01@gmail.com>
2016-01-16 07:10:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/cardviewsample/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.cardviewsample;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.github.brnunes.swipeablerecyclerview.SwipeableRecyclerViewTouchListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        // コンテンツの変化でRecyclerViewのサイズが変わらない場合は、
+        // パフォーマンスを向上させることができる
+        mRecyclerView.setHasFixedSize(true);
+
+        // LinearLayoutManagerを使用する
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        // テストデータを作成する
+        final ArrayList<String> myDataSet = new ArrayList<String>() {
+            {
+                add("北海道");
+                add("青森県");
+                add("茨城県");
+                add("新潟県");
+                add("三重県");
+                add("鳥取県");
+                add("徳島県");
+                add("福岡県");
+                add("沖縄県");
+                add("岩手県");
+                add("栃木県");
+                add("富山県");
+                add("滋賀県");
+                add("島根県");
+                add("香川県");
+                add("佐賀県");
+            }
+        };
+
+        // アダプタを指定する
+        mAdapter = new SampleAdapter(this, myDataSet);
+        mRecyclerView.setAdapter(mAdapter);
+
+        SwipeableRecyclerViewTouchListener swipeTouchListener =
+                new SwipeableRecyclerViewTouchListener(mRecyclerView,
+                        new SwipeableRecyclerViewTouchListener.SwipeListener() {
+                            @Override
+                            public boolean canSwipe(int position) {
+                                return true;
+                            }
+
+                            @Override
+                            public void onDismissedBySwipeLeft(RecyclerView recyclerView, int[] reverseSortedPositions) {
+                                for (int position : reverseSortedPositions) {
+                                    myDataSet.remove(position);
+                                    mAdapter.notifyItemRemoved(position);
+                                }
+                                mAdapter.notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onDismissedBySwipeRight(RecyclerView recyclerView, int[] reverseSortedPositions) {
+                                for (int position : reverseSortedPositions) {
+                                    myDataSet.remove(position);
+                                    mAdapter.notifyItemRemoved(position);
+                                }
+                                mAdapter.notifyDataSetChanged();
+                            }
+                        });
+
+        mRecyclerView.addOnItemTouchListener(swipeTouchListener);
+    }
+
+}
